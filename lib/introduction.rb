2295ae77ca5d68eb36a202501a9d706edf01b500
Elias Delgado <evdelgado99@gmail.com>
2020-08-07 19:38:18 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+# introduce Josh using a name that out uts a phrase with that name using string interpolation
+
+def introduction (name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
