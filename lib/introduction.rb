9b6907376b0876397c6a9b9526486a7ae316e214
chaim chazan <chaimyisroel@gmail.com>
2021-06-04 22:19:59 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+introduction("Josh")
+
+def introduction_with_language(name, lan)
+  puts "Hi, my name is #{name} and I am learning to program in #{lan}.\n"
+end
+introduction_with_language("Josh", "Ember.js")
